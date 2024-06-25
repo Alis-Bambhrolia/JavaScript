e5d959dde8a87fd610d1301bf4ebf4f393ba82a4
Alis-Bambhrolia <alisbambhrolia12@gmail.com>
2024-06-25 14:44:08 +0530

    Add files via upload

---
+++ 01_basic.js
@@ -0,0 +1,16 @@
+const accountId = 123456 // const mean can't change the value
+let accountemail = "hiIamANIME@gmail"
+var password = "JohnWick123" // do not use it 
+accountCity = "New York"
+let accountState;
+
+/* 
+do not use var
+*/
+
+console.log(accountId);
+console.log(accountemail);
+console.log(password);
+console.log(accountCity);
+
+console.table([accountId, accountemail, password, accountCity, accountState])
