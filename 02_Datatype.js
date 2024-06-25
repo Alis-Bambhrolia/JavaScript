24a02b2b9b150030434aa37a3253acae8593338d
Alis-Bambhrolia <alisbambhrolia12@gmail.com>
2024-06-25 22:09:38 +0530

    Add files via upload

---
+++ 02_Datatype.js
@@ -0,0 +1,43 @@
+"use strict"; // treat all js code as newer version
+
+// alert(96 + 4)  // we are using nodejs , not the browser
+
+console.log( 3 + 6 );
+console.log( "Hi , i am good" );
+
+// DataTypes 
+
+let name = "IRON MAN" // string datatype
+let age = 25 // number datatype 
+let islogin = true // boolean datatype --> true or false
+let state = null
+let accountState;  // undefined 
+
+
+// Number ==> 2 to the power 53 
+// BigInt ==> let anotherHugeNumber = BigInt("900719925474099300000000000000000");
+
+// String ==> use "" 
+// boolean ==> true or false only 
+// null ==> standalone value 
+// undefined 
+// Symbol ==> unique
+
+// object  
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof islogin);
+console.log(typeof state); 
+console.log(typeof accountState);  // typeof checks if the value is a number, string, or another type.
+// REMEMBER The value "123456" is enclosed in quotes, making it a string. If you want it to be a number, you should not use quotes.
+
+
+
+
+
+
+
+
+
+
+
